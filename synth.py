b1703c07a5d1bbd294af208edc7c4d8efb6f3391
Conor Lawless <cnr.lwlss@gmail.com>
2020-02-16 17:33:31 +0000

    Create synth.py

---
+++ synth.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def update_w(w0, Fext, cFriction, M, dt):
+    return(w0 + (float(Fext - w0*cFriction)/float(M))*dt)
+
+def update_theta(theta0, w0, Fext, cFriction, M, dt):
+    return(theta0 + w0*dt - float(Fext - w0*cFriction)/float(M)*(dt**2))
+
+def update(theta0, w0, Fext, cFriction, M, dt):
+    w = update_w(w0, Fext, cFriction, M, dt)
+    theta = update_theta(theta0, w0, Fext, cFriction, M, dt)
+    return((w,theta))
+
+
+Fext = 1
+cFriction = 1
+M = 1
+t = 0
+
+Nsamps = 100
+dt = 0.1
+res = np.zeros(Nsamps)
+
+w = 0.0
+theta = 0.0
+
+for i in range(0,Nsamps):
+ x = np.sin(theta)
+ res[i] = x
+ #print(i,t,w,theta,x)
+ t = t+dt
+ w,theta = update(theta,w,Fext, cFriction,M,dt)
+
+print(res)
+
+
+
